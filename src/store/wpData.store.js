a0ee7db6eb69bebf7bebe289364f7164bd60b1b0
Alex <alxsar98@gmail.com>
2021-07-30 16:05:03 +0300

    feat: loader in store. New logic get posts images

--- src/store/wpData.store.js
+++ src/store/wpData.store.js
@@ -1,61 +1,50 @@
-import API_URL  from "@/boot/api";
+import API_URL from "@/boot/api";
 
 const state = {
-  // messages: [],
+  posts: null,
+  loader: false
 };
 
 const getters = {};
 
-const actions = {
-  loadPosts() {
-    return new Promise((resolve, reject) => {
-      API_URL.get("posts")
-        .then((response) => {
-          resolve(response);
-        })
-        .catch((error) => {
-          reject(error);
-        });
-    });
+
+const mutations = {
+  SET_POSTS(state, payload) {
+    state.posts = payload;
   },
 
-  loadImagePost(_, data) {
-    return new Promise((resolve, reject) => {
-      API_URL.get(`media/${data.id}`)
-        .then((response) => {
-          resolve(response);
-        })
-        .catch((error) => {
-          reject(error);
-        });
-    });
+  SET_LOADER_TRUE() {
+    state.loader = true
   },
 
-  loadOncePost(_, data) {
-    return new Promise((resolve, reject) => {
-      API_URL.get(`media?parent=${data.id}`)
-        .then((response) => {
-          resolve(response)
-        })
-        .catch((error) => {
-          reject(error)
-        })
-    })
+  SET_LOADER_FALSE() {
+    state.loader = false
   }
 };
 
-const mutations = {
-  /*
-        SET_MESSAGES(state, payload) {
-            state.messages = payload;
-        }
-     */
+const actions = {
+  loadPosts({ commit }) {
+    commit("SET_LOADER_TRUE")
+    return new Promise((resolve, reject) => {
+      API_URL.get("posts")
+        .then((response) => {
+          console.log(response.data);
+          commit("SET_POSTS", response.data)
+          commit("SET_LOADER_FALSE")
+          resolve(response);
+        })
+        .catch((error) => {
+          reject(error);
+        });
+    });
+  },
 };
+
 
 export default {
   namespaced: true,
   state,
   getters,
   actions,
-  mutations,
+  mutations
 };
